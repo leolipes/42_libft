97b8ed783a175543a35c0d56bbf62b6931b705e5
lfilipe- <leonardofilipenasci@gmail.com>
2021-07-29 14:52:54 -0300

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: leolipes <leolipes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/29 09:35:20 by leolipes          #+#    #+#             */
-/*   Updated: 2021/07/29 14:38:38 by leolipes         ###   ########.fr       */
+/*   Created: 2021/07/29 14:14:04 by leolipes          #+#    #+#             */
+/*   Updated: 2021/07/29 14:52:07 by leolipes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	*ft_memset(void *str, int c, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int	i;
-	unsigned char	*ptr;
 
 	i = 0;
-	ptr = (unsigned char *)str;
 	while (i < n)
 	{
-		ptr[i] = (unsigned char)c;
+		((char *)dest)[i] = ((char *)src)[i];
 		i++;
 	}
-	return (str);
+	return (dest);
 }
