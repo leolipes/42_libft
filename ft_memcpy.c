d7c6ca8c5f5bc0f2e8fc54f66292a8985f294fc5
lfilipe- <leonardofilipenasci@gmail.com>
2021-08-14 14:15:25 -0300

    corrected ft_memcpy again

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: leolipes <leolipes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 14:14:04 by leolipes          #+#    #+#             */
-/*   Updated: 2021/08/14 13:43:25 by leolipes         ###   ########.fr       */
+/*   Updated: 2021/08/14 14:14:45 by leolipes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!dest || !src)
+	if (!dest && !src)
 		return (0);
 	while (i < n)
 	{
