fce1738357ba8901eb4b2fbfed9a90b798de14dd
lfilipe- <leonardofilipenasci@gmail.com>
2021-08-21 17:22:13 -0300

    ft_lstnew protected

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: leolipes <leolipes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 12:16:05 by leolipes          #+#    #+#             */
-/*   Updated: 2021/08/21 16:17:10 by leolipes         ###   ########.fr       */
+/*   Updated: 2021/08/21 17:21:35 by leolipes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ t_list	*ft_lstnew(void *content)
 	t_list	*element;
 
 	element = malloc(sizeof(t_list));
+	if (!element)
+		return (0);
 	element->content = content;
 	element->next = NULL;
 	return (element);
